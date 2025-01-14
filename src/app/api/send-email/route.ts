447b6aef14dc4d1fcef6a648c9845500d60e9ca7
Ethan Knotts <eknotts64@gmail.com>
2025-01-14 16:49:09 -0500

    Added email functionality.
    Added a function in the main component to convert the URL in processedImage to a Base64 encoded string, which is passed on to the API.

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server';
-import { createTransporter } from '@/utils/email';
+import { transporter } from '@/utils/email';
 
 export async function POST(request: Request) {
+  console.log("Request", request);
+
   try {
     const { email, image } = await request.json();
-
-    const transporter = createTransporter();
+    
+    console.log('Sending email to:', email);
+    console.log("Image", image);
 
     await transporter.sendMail({
       from: process.env.SMTP_USER,
